56d77234bdc8356d209102bdcbe05a32f98bba6b
Sebastian Linero <sebastianlinero15@outlook.com>
2025-09-11 14:12:34 -0500

    Add initial project files and basic exercises

---
+++ exercises2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int age = 24;
+string name = "Sebastian";
+Console.WriteLine($"Name: {name}, Age: {age}");
---
+++ exercises_3/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string capitalLetters = "this text have in lower casse" .ToUpper();
+Console.WriteLine(capitalLetters);
