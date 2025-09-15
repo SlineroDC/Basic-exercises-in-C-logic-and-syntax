24b59a5be39f06bbc71c56301b75fccdbad15040
Sebastian Linero <sebastianlinero15@outlook.com>
2025-09-15 17:22:18 -0500

    Update assembly informational version for exercises

--- exercises_16/obj/Debug/net8.0/exercises_16.AssemblyInfo.cs
+++ exercises_16/obj/Debug/net8.0/exercises_16.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercises_16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d77234bdc8356d209102bdcbe05a32f98bba6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aacb443b1deabf980d73b1e806b155feb6e69c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercises_16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercises_16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
